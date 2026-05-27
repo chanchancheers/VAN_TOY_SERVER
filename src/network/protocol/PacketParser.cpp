a7f870225e6ebcc6682a7b8c5313727aaa23bc1d
신은찬 <sineuncha@naver.com>
2026-05-27 17:31:03 +0900

    [feat] PacketParser - 파싱 함수 작성

--- src/network/protocol/PacketParser.cpp
+++ src/network/protocol/PacketParser.cpp
@@ -24,3 +24,40 @@ unsigned int PacketParser::findPacketStart(Buffer &buffer) {
     else buffer.consume(pos + 1);
     return -1;
 }
+
+unsigned int PacketParser::parseLength(Buffer &buffer, unsigned int pos) {
+    const uint8_t* p = buffer.peek() + pos;
+    return p[pos] << 8 | p[pos + 1];
+}
+
+bool PacketParser::findPacketEnd(Buffer& buffer, unsigned int body_start, unsigned int length) {
+    const uint8_t* p = buffer.peek() + body_start + length;
+    return p[0] == 0x50 && p[1] == 0x45;
+}
+
+
+
+std::optional<VANProtocol> PacketParser::parse(Buffer& buffer) {
+    unsigned int packet_start;
+    unsigned int length_start;
+    unsigned int body_start;
+    if ((packet_start = findPacketStart(buffer)) != -1) {
+        length_start = packet_start + VANProtocol::STX_LENGTH;
+    } else {
+        return std::nullopt;
+    }
+    unsigned int length = parseLength(buffer, length_start);
+    body_start = length_start + VANProtocol::LEN_LENGTH;
+
+
+    if (findPacketEnd(buffer, body_start, length)) {
+        VANProtocol packet;
+        packet.setLength(length);
+        packet.setData(std::vector<uint8_t>(buffer.peek() + packet_start,
+            buffer.peek() + packet_start + length + VANProtocol::ALL_LENGTH));
+        return packet;
+    } else {
+        return std::nullopt;
+    }
+
+}
