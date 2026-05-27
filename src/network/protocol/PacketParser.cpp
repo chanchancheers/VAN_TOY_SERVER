2d6b96bd90065bd809a683eb56614ac4d0b1b330
신은찬 <sineuncha@naver.com>
2026-05-27 17:28:32 +0900

    [fix] PacketParser STX 탐색 함수 리턴값 변경

--- src/network/protocol/PacketParser.cpp
+++ src/network/protocol/PacketParser.cpp
@@ -8,15 +8,15 @@
 /**
  * 패킷의 시작점을 탐색
  * @param buffer
- * @return Packet을 찾았을 경우 STX 이후의 위치를 반환 / 없을 경우 -1
+ * @return Packet을 찾았을 경우 STX 위치를 반환 / 없을 경우 -1
  */
-int PacketParser::findPacketStart(Buffer &buffer) {
+unsigned int PacketParser::findPacketStart(Buffer &buffer) {
     const uint8_t* p = buffer.peek();
     size_t pos = 0;
     while (pos + 1 < buffer.readableBytes()) {
         if (p[pos] == 0x50 && p[pos+1] == 0x53) {
             buffer.consumeWithExtPos(pos, pos);
-            return pos + 2;
+            return pos;
         }
         pos++;
     }
